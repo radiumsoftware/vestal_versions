9807729c5b0f1d72b400db10b387cac8133330ff
Adman65 <adman1965@gmail.com>
2011-06-11 11:49:27 -0700

    Don't notify observers about initial version

--- lib/vestal_versions/observing.rb
+++ lib/vestal_versions/observing.rb
@@ -5,7 +5,7 @@ module VestalVersions
       klass.class_eval do
         include InstanceMethods
 
-        after_update :notify_observers_of_created_version, :if => :version_conditions_met?
+        after_update :notify_observers_of_created_version, :if => :version_conditions_met?, :unless => :initial_version?
       end
 
     end
@@ -15,6 +15,10 @@ module VestalVersions
       def notify_observers_of_created_version
         self.class.changed
         self.class.notify_observers(:version_created, self)
+      end
+
+      def initial_version?
+        version == 1
       end
     end
 
